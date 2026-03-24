c3caffd4289fed0f9cab261a38a6baa83dae7a3d
abhishek-ukani <abhishekpatel03102025@gamil.com>
2026-03-24 12:36:35 +0530

    fix changes

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -21,8 +21,9 @@ export default function EditInvoiceForm({
 }) {
     const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
 
-  return <form action={updateInvoiceWithId}></form>;
-      {/* <div className="rounded-md bg-gray-50 p-4 md:p-6">
+  return (
+    <form action={updateInvoiceWithId}>
+      <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
@@ -120,4 +121,7 @@ export default function EditInvoiceForm({
           Cancel
         </Link>
         <Button type="submit">Edit Invoice</Button>
-      </div> */}
+      </div>
+    </form>
+  );
+}
